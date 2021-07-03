516b66fa55db3bfc89ed6c65a14fafafbec9485b
Alex <alexrogersdesign@gmail.com>
2021-07-03 10:18:45 -0400

    working on directory change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import './App.css';
 import React, { useReducer, useRef,  useState } from 'react';
 import * as mobilenet from '@tensorflow-models/mobilenet'; 
+import * as tf from '@tensorflow/tfjs-node';
+import {loadLayersModel} from '@tensorflow/tfjs-converter';
 
 import state from './state';
 
-const reducer = (currentState, event) =>
- state.states[currentState].on[event] || state.initial;
-
+const modelUrl = {
+  model: 'https://arogerscapstone.blob.core.windows.net/capstone-tensorflow',
+  };
 
 function App() {
+  const reducer = (currentState, event) =>
+  state.states[currentState].on[event] || state.initial;
   const [appState, dispatch] = useReducer(reducer, state.initial);
   const next = () => dispatch("next"); 
   const [model, setModel] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [results, setResults] = useState([]);
   const inputRef = useRef();
   const imageRef = useRef();
-  const [imageUrl, setImageUrl] = useState(null);
+
+  const load = async () => {
+    next()
+    const model = await tf.loadLayersModel(modelUrl);
+    setModel(model)
+    next()
+  }
+
+  const indentify = async () => {
+    next();
+    const results = await model.classify(imageRef.current);
+    setResults(results)
+    next()
+  }
 
   const handleUpload = event => {
     const { files } = event.target;
@@ -25,27 +44,43 @@ function App() {
     }
   }
 
-  const load = async () => {
-    next()
-    const mobilenetModel = await mobilenet.load();
-    setModel(mobilenetModel)
-    next()
+
+  const formatResult = ({ className, probability}) => (
+    <li key={className}>
+      {'${className}: %s{(probability * 100).toFixed(2)}'}
+    </li>
+  )
+  const resetState = () => {
+    setImageUrl([])
+    setResults([])
+    next();
   }
 
   const buttonProps = {
-    initial: { text: "Load Model", action: load},
-    loadingModel: {text: "Loading Model" , action: ()=> {},
+    initial: { text: "Load Model", action: load },
+    loadingModel: { text: "Loading Model" , action: ()=> {} },
     modelReady: { text: "Upload Image", action: ()=> inputRef.current.click() },
-    imageReady: { text: "Identify Fire", action: () => {} },
+    imageReady: { text: "Identify Fire", action: indentify},
     identifying: { text: "Identifying", action: () => {} },
-    complete: { text: "Reset", action: () => {} }
+    complete: { text: "Reset", action: resetState }
     }
-  }
-  const { showImage = false } = state.states[appState]
+
+
+  const { showImage = false , showResults = false} = state.states[appState]
   return (
     <div className="App">
-      <button onClick={buttonProps[appState].action}>{buttonProps[appState.text]}</button>
+      { showImage &&  <img src={imageUrl} alt="file-preview" ref={imageRef}/>}
+      <button onClick={buttonProps[appState].action}>{buttonProps[appState].text}</button>
       <input type="file" accept="image/*" capture="camera" ref={inputRef} onChange={handleUpload}></input>
+      {
+        results.map(({ className, probability }) => (
+          <li key={className}>
+            {`${className}: %${(probability * 100).toFixed(
+              2
+            )}`}
+          </li>
+        ))
+      }
     </div>
   );
 }
