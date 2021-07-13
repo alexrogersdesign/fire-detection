a9853ff732209181553db60b394737340d8e9cc4
alexrogersdesign <51722825+alexrogersdesign@users.noreply.github.com>
2021-07-13 15:21:07 -0400

    prepping for heroku

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import MainPage from 'components/Actual/MainPage';
 import PredictWrapper from 'components/Actual/PredictWrapper';
-import Notebook from 'components/Actual/Notebook';
 
 export default function App() {
   return (
@@ -14,9 +13,6 @@ export default function App() {
     <Switch>
       <Route path="/predict">
         <PredictWrapper/>
-      </Route>
-      <Route path="/notebook">
-        <Notebook/>
       </Route>
       <Route path="/">
         <MainPage/>
